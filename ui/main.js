2f181a34fab6a18215e87d4f6118296b32ff0e69
anu2108 <30805325+anu2108@users.noreply.github.com>
2017-08-13 18:03:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-  
-  
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
     
+    request.onreadystatechange = function()
+    {
+      if(request.readystate === XMLHttpRequest.done)  {
+          if(request.status === 200)
+          {
+              var counter = request.responseText();
+              var span = document.getElementById('count');
+              span.innerHTML= counter.toString();
+          }
+      }
+    };
+  
+  
+  request.open('GET','http://anubhavtendulkar.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
