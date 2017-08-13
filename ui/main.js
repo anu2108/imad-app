39e8a4474e588bef2daf1d860c78acd690aeee0d
anu2108 <30805325+anu2108@users.noreply.github.com>
2017-08-13 12:56:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ element.innerHTML = 'Waaaaasssssaaaaaap';
 var img = document.getElementById('madi') ;
 var marginLeft = 0;
 function moveright (){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function(){
-  var interval = setInterval (moveright, 100);
+  var interval = setInterval (moveright, 50);
 };
