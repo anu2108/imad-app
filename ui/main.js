524799f087f35fb712746725a2d9c312f43d8f6d
anu2108 <30805325+anu2108@users.noreply.github.com>
2017-08-13 13:44:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,10 @@
-console.log('Loaded!');
-var element = document.getElementById('main text'
-);
-element.innerHTML = 'Waaaaasssssaaaaaap';
-
-var img = document.getElementById('madi') ;
-var marginLeft = 0;
-function moveright (){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-
-img.onclick = function(){
-  var interval = setInterval (moveright, 50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+  
+  
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+    
 };
